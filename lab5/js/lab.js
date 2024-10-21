e6d2e6a3d32f1f9466d97742f1647835d56c9eed
Drew Vander Weele <drewvanderweele@gmail.com>
2024-10-21 13:38:07 -0700

    Update 2 Lab 5

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,21 +1,20 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Lab 5: Data Types and Variables 
+// Author: Drew Vander Weele 
+// Date: 10/21/24
 
-// Constants
+// Define Variables
+make = "Subaru";
+model = "ForesterWilderness";
+color = "LightBlue";
+year = 2024;
 
-// Functions
+// calculate
+age = 2024 - year;
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
+// output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
