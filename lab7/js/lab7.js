f9a9c7c804f6f9ebd502eded8d5ae6103f4276c3
Drew Vander Weele <drewvanderweele@gmail.com>
2024-10-28 18:31:11 -0700

    Update lab7.js

--- lab7/js/lab7.js
+++ lab7/js/lab7.js
@@ -1,18 +1,22 @@
 // sortUserName - a function that takes a users input and scrambles
 // the letter of their name
+
 function sortUserName() {
+
     var userName = window.prompt("Hi there! Please type in your name so I can scramble it!");
     console.log("userName =", userName);
-    // split string array 
+    
+
     var nameArray = userName.split('');
-    console.log("nameArray =", nameArray);
-    // sort the array
+    console.log("userName =", userName);
+    
     var nameArraySort = nameArray.sort();
     console.log("nameArraySort =", nameArraySort);
-    // join array back to a string
+
     var nameSorted = nameArraySort.join('');
     console.log("nameSorted =", nameSorted);
     return nameSorted;
-    // output 
-    document.writeln("Horray! I fixed your name!: ", sortUserName(), "</br>");
 }
+
+// output 
+document.writeln("Horray! I fixed your name!: ", sortUserName(), "</br>");
