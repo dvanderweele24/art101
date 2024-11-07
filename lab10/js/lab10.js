8c9f03e1f9fc2173bb971d52eea5f215d410b80d
Drew Vander Weele <drewvanderweele@gmail.com>
2024-11-07 15:25:34 -0800

    Update Lab 10

---
+++ lab10/js/lab10.js
@@ -0,0 +1,26 @@
+/*
+   lab.js - This simple Javascript/jQuery script appends new elements to an output div
+   Author: Drew Vander Weele 
+   Date: 11/7/24
+*/
+
+function generateRandomText(){
+    const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+    const min = 3;
+    const max = 100;
+    const randLen = Math.floor(Math.random() * (max - min 1))+min;
+    // Get a random starting index to slice the Lorem ipsum text
+    const randStart = Math.floor(Math.random() * (text.length - randLen + 1));
+    // Generate the random Lorem ispum-like text
+    return text.slice(randStart, randStart, randLen);
+}
+
+//click listener for button 
+$("make-convo").click(function(){
+});
+
+//get new fake dialogue
+const newText = generateRandomText();
+
+//append a new div to our output div 
+$("#output").append('<div class="text"><p>' + newText + '</p></div>');
